e2a44f18673abe535d922f921143c3aee2e11234
IvanTrosic <ivtrosic@vsite.hr>
2024-12-01 08:22:20 +0100

    c

---
+++ BookAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+
+namespace BookAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        
+        public AppDbContext(DbContextOptions<AppDbContext> options) :
+        base(options) { }
+        public DbSet<Book> Books
+        { get; set;}
+    }
+}
